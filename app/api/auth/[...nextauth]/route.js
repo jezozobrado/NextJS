7e3e0ecfbc897768bfacb6329bcfa9b20c3711f2
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-06-06 00:15:54 +0800

    Fix database connection.

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,36 +11,37 @@ const handler = NextAuth({
     }),
   ],
 
-  async session({ session }) {
-    const sessionUser = await User.findOne({ email: session.user.email });
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
 
-    session.user.id = sessionUser._id.toString();
+      session.user.id = sessionUser._id.toString();
 
-    return session;
-  },
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-
-      //check if user exists
-      const userExists = await User.findOne({
-        email: profile.email,
-      });
-
-      if (!userExists) {
-        await User.create({
+        //check if user exists
+        const userExists = await User.findOne({
           email: profile.email,
-          username: profile.username.replace(" ", "").toLowerCase(),
-          image: profile.picture,
         });
-      }
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
 
